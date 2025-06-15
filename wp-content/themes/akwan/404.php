49dff23e00e3f68423bb1d94dd6f2c99d74ca0c9
Youssef Mohamed <hendaoye.201250@gamil.com>
2025-06-15 17:29:42 +0300

    feat : our_projects_block updates swiper

--- wp-content/themes/akwan/404.php
+++ wp-content/themes/akwan/404.php
@@ -26,18 +26,34 @@ if (isset($block)) {
 ?>
 <div class="page-not-found animation-fade-me-up">
   <div class="container">
-    <div class="content-wrapper column">
-          <h1  class="main-title text-center sky-breeze fw-600">
-            404
-          </h1>
-          <div class="description paragraph-18 text-center sky-breeze">
-            Sorry, the page you are looking for could not be found. It might have been removed, renamed, or did not exist in the first place.
-          </div>
-        <a href="<?php site_url(); ?>"
-           class="cta-button main-cta-button light" >
-          Back to Home
-        </a>
-    </div>
+      <div class="content-wrapper column">
+          <?php
+          $lang = function_exists('pll_current_language') ? pll_current_language() : get_locale();
+          ?>
+
+          <?php if ($lang === 'ar' || $lang === 'ar_EG'): ?>
+              <h1 class="main-title text-center sky-breeze fw-600">
+                  404
+              </h1>
+              <div class="description paragraph-18 text-center sky-breeze">
+                  عذرًا، الصفحة التي تبحث عنها غير موجودة. ربما تم حذفها، تغيير اسمها، أو لم تكن موجودة من الأساس.
+              </div>
+              <a href="<?php echo site_url(); ?>" class="cta-button main-cta-button light">
+                  العودة إلى الصفحة الرئيسية
+              </a>
+          <?php else: ?>
+              <h1 class="main-title text-center sky-breeze fw-600">
+                  404
+              </h1>
+              <div class="description paragraph-18 text-center sky-breeze">
+                  Sorry, the page you are looking for could not be found. It might have been removed, renamed, or did not exist in the first place.
+              </div>
+              <a href="<?php echo site_url(); ?>" class="cta-button main-cta-button light">
+                  Back to Home
+              </a>
+          <?php endif; ?>
+      </div>
+
   </div>
 </div>
 <?php get_footer(); ?>
